81b542ee7330a6f8f64245835afc5f59c7dd5993
Daniel W. Siebo <danielwsiebo30@gmail.com>
2025-09-16 23:22:48 +0000

    Initial commit

---
+++ basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+sum_of_numbers = number1 + number2
+remainder_of_numbers = number1 - number2
+difference_of_numbers = number1 / number2
+product_of_numbers  = number1 * number2
+print("sum:", sum_of_numbers)
+print("Remainder:", remainder_of_numbers)
+print("Quotient:", difference_of_numbers)
+print("Product:", product_of_numbers)
