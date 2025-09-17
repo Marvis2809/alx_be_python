40d905a45b659f8c4b72a0375878228b39d198a0
Daniel W. Siebo <danielwsiebo30@gmail.com>
2025-09-17 21:19:48 +0000

    Move simple_interest.py into python_introduction directory

---
+++ basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+sum_of_numbers = number1 + number2
+difference_of_numbers = number1 - number2
+product_of_numbers  = number1 * number2
+print("Sum of", number1, "and", number2, "is", sum_of_numbers)
+print("Difference of", number1, "and", number2, "is", difference_of_numbers)
+print("Product of", number1, "and", number2, "is", product_of_numbers)
