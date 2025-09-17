189165f57992e6636f73c9c74acd0fbc4fe98d5d
Daniel W. Siebo <danielwsiebo30@gmail.com>
2025-09-17 14:18:21 +0000

    Fix: correct arithmetic operations and print format

--- basic_operations.py
+++ basic_operations.py
@@ -1,10 +1,8 @@
 number1 = 10
 number2 = 5
 sum_of_numbers = number1 + number2
-remainder_of_numbers = number1 - number2
-difference_of_numbers = number1 / number2
+difference_of_numbers = number1 - number2
 product_of_numbers  = number1 * number2
-print("sum:", sum_of_numbers)
-print("Remainder:", remainder_of_numbers)
-print("Quotient:", difference_of_numbers)
-print("Product:", product_of_numbers)
+print("Sum of", number1, "and", number2, "is", sum_of_numbers)
+print("Difference of", number1, "and", number2, "is", difference_of_numbers)
+print("Product of", number1, "and", number2, "is", product_of_numbers)
