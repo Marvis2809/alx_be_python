80f2cc28520d49b736c7dad2ffa4642da0d24861
Daniel W. Siebo <danielwsiebo30@gmail.com>
2025-10-01 23:18:54 +0000

    Add changes to match_case_calculator.py

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -2,18 +2,21 @@ num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 operation = input("Choose the operation (+, -, *, /): ")
 
-if operation == "+":
-    result = num1 + num2
-elif operation == "-":
-    result = num1 - num2
-elif operation == "*":
-    result = num1 * num2
-elif operation == "/":
-    result = num1 / num2
-else:
-    print("Invaild operation.")
-    result = None
-
+match operation:
+    case "+":
+        result = num1 + num2
+    case "-":
+        result = num1 - num2
+    case"*":
+        result = num1 * num2
+    case "/":
+        if num1 or num2 == 0:
+            print("Cannot divide by zero.")
+            result = None
+        else:
+            num1 / num2
+    case _:
+        result = None
 if result is not None:
     if isinstance(result, float) and result.is_integer():
         print(f"The result is {int(result)}.")
