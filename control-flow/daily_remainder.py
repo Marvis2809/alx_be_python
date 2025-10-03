ff4d61a6cd023376bea0e81ba1de73be15f9d695
Daniel W. Siebo <danielwsiebo30@gmail.com>
2025-10-03 00:36:25 +0000

    Add daily_remainder.py into control-flow

--- control-flow/daily_remainder.py
+++ control-flow/daily_remainder.py
@@ -1,15 +1,15 @@
 task = input("Enter the task description: ")
 priority = input("Enter the task (high, meduim, low): ")
-time_bound = input("Is there task time bound (yes or no): ")
+time_bound = input("Is there task time-bound? (yes or no): ")
 match priority:
     case "high":
-        remainder =f"Remainder: '{task}' is a HIGH priority task. "
+        remainder =f"Remainder: '{task}' is a HIGH priority task"
     case "medium":
-        remainder =f"Remainder: '{task}' is a MEDIUM priority task. "
+        remainder =f"Remainder: '{task}' is a MEDIUM priority task"
     case "low":
-        remainder =f"Remainder: '{task}' is a LOW priority task. "
+        remainder =f"Remainder: '{task}' is a LOW priority task"
     case _:
-        remainder =f"Remainder: '{task}' has an unspecified priority. "
+        remainder =f"Remainder: '{task}' has an unspecified priority"
 if time_bound =="yes":
-    remainder += "It requires immediate attention today!"
+    remainder += "that requires immediate attention today!"
 print(remainder)
